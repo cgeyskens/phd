dc67227aa538200ede42b4cc696f0d18bc3844c3
cgeyskens <cydric.geyskens@gmail.com>
2025-12-15 20:03:38 +0100

    update synapse counting script: 06 for paper

--- image-analysis/synapse-counting/notebooks/06_plot_single_metric.R
+++ image-analysis/synapse-counting/notebooks/06_plot_single_metric.R
@@ -1,41 +1,43 @@
 library(dplyr)
 library(ggplot2)
 
+#### =============================== arguments =============================== ####
+synapse = "VGAT-GEPH"
+layer = "CA1 SLM"
+metric = "pre_puncta_density_per_100_um2"
 
 
-
-
-# import data
+#### =============================== get input =============================== ####
 file_path_vglut1 <- "/mnt/image-analysis/synapse-counting/IHC_Exp9_VCAM1_VGLUT1-PSD95_output_data_20251201_194547/metric_results.csv"
-df_vglut1 = read.csv(file_path_vglut1)
+file_path_vgat <- "/mnt/image-analysis/synapse-counting/IHC_Exp12_VCAM1_VGAT-GEPH_output_data_20251201_194539/metric_results.csv"
 
+if (synapse == "VGLUT1-PSD95"){
+  data = read.csv(file_path_vglut1)
+} else if (synapse == "VGAT-GEPH") {
+  data = read.csv(file_path_vgat)
+}
+
+
+#### =============================== data processing =============================== ####
 # subset data
-ca1_sr_data_local_peak_max <- df_vglut1 %>%
-  filter(hippocampal_layer == "CA1 SR") %>%
-  select(local_peak_colocalized_spots, gRNA, Brain)
+metric_data <- data %>%
+  filter(hippocampal_layer == layer) %>%
+  select(all_of(metric), gRNA, Brain)
 
-# get the LacZ global value
-lacz_global <- ca1_sr_data_local_peak_max %>%
-  filter(gRNA == "LacZ-gRNA") %>%
-  summarise(m = mean(local_peak_colocalized_spots, na.rm = TRUE)) %>%
-  pull(m)
+# # get the LacZ global value
+# lacz_global <- ca1_sr_data_local_peak_max %>%
+#   filter(gRNA == "LacZ-gRNA") %>%
+#   summarise(m = mean(local_peak_colocalized_spots, na.rm = TRUE)) %>%
+#   pull(m)
 
-# normalize
-df_norm <- ca1_sr_data_local_peak_max %>%
-  mutate(local_peak_colocalized_spots_norm = local_peak_colocalized_spots / lacz_global)
+# # normalize
+# df_norm <- ca1_sr_data_local_peak_max %>%
+#   mutate(local_peak_colocalized_spots_norm = local_peak_colocalized_spots / lacz_global)
 
-# get brain mean
-df <- ca1_sr_data_local_peak_max %>%
+# get brain-level means
+brain_means <- metric_data %>%
   group_by(gRNA, Brain) %>%
-  summarise(brain_mean = mean(local_peak_colocalized_spots, na.rm = TRUE),
-            .groups = "drop")
-library(dplyr)
-library(ggplot2)
-
-# brain-level means
-brain_means <- ca1_sr_data_local_peak_max %>%
-  group_by(gRNA, Brain) %>%
-  summarise(brain_mean = mean(local_peak_colocalized_spots, na.rm = TRUE),
+  summarise(brain_mean = mean(.data[[metric]], na.rm = TRUE),
             .groups = "drop")
 
 # global LacZ mean
@@ -45,9 +47,9 @@ lacz_global <- brain_means %>%
   pull(m)
 
 # FOV-level normalized + numeric x
-fov_df <- ca1_sr_data_local_peak_max %>%
+fov_df <- metric_data %>%
   mutate(
-    y_norm = local_peak_colocalized_spots / lacz_global,
+    y_norm = .data[[metric]] / lacz_global,
     x = ifelse(gRNA == "LacZ-gRNA", 1, 2)
   )
 
@@ -56,8 +58,16 @@ brain_df <- brain_means %>%
   mutate(
     y_norm = brain_mean / lacz_global,
     x = ifelse(gRNA == "LacZ-gRNA", 1, 2),
-    xn = ifelse(gRNA == "LacZ-gRNA", 1 + 0.12, 2 - 0.12)
+    xn = ifelse(gRNA == "LacZ-gRNA", 1 + 0.2, 2 - 0.2)
   )
+
+
+#### =============================== plotting =============================== ####
+# specifying the colors
+grna_cols <- c(
+  "VCAM1-gRNA" = "#21a0e2",
+  "LacZ-gRNA" = "#c6c6c6"
+)
 
 p <- ggplot() +
   # FOV points (outside-ish via jitter)
@@ -65,35 +75,60 @@ p <- ggplot() +
     data = fov_df,
     aes(x = x, y = y_norm, color = gRNA),
     # position = position_jitter(width = 0.10, height = 0),
-    alpha = 0.75, size = 2
+    alpha = 0.6, size = 5
   ) +
   # paired brain means line (inside)
   geom_line(
     data = brain_df,
     aes(x = xn, y = y_norm, group = Brain),
-    linewidth = 0.6, color = "black"
+    linewidth = 0.8, color = "black"
   ) +
   # brain mean points (inside, colored, different shape)
   geom_point(
     data = brain_df,
     aes(x = xn, y = y_norm, fill = gRNA),
-    shape = 21, color = "black", size = 3.2, stroke = 1
+    shape = 21, size = 8, stroke = 0, alpha = 1
   ) +
   scale_x_continuous(
   ) +
   labs(
     x = NULL,
-    y = "local_peak_colocalized_spots / global LacZ mean"
+    y = metric
   ) +
+  scale_color_manual(
+    values = grna_cols
+  ) + 
+  scale_fill_manual(
+     values = grna_cols
+  ) + 
   theme_classic() +
-  coord_cartesian(xlim = c(0.7, 3)) +
+  coord_cartesian(xlim = c(0.7, 2.2)) +
   theme(
     legend.position = "none",
+    panel.background = element_blank(),
+    panel.grid = element_blank(), 
+    axis.line = element_line(color = "black", size = 1.2),  
     axis.ticks.x = element_blank(),
-    ) + 
+    axis.ticks.y = element_line(color = "black", size = 1.2),
+    axis.ticks.length.y = unit(.3, "cm"),
+    axis.text = element_text(size = 14), 
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 16)
+    )  +
+    scale_y_continuous(
+        expand = c(0, 0),
+        limits = c(0, 2),
+        breaks = c(0, 0.5, 1, 1.5, 2) 
+    ) +  
+    scale_x_continuous(
+        expand = c(0, 0),
+        limits = c(0.8, 2.2),
+        breaks = c(0, 1, 2) 
+    ) +
+    coord_fixed(ratio = 2) 
 p
 
-ggsave(paste0("VGLUT1-PSD95_CA1_SR_metric_paper.svg"), 
+ggsave(paste0(synapse, "_", layer, "_", metric, "_paper.svg"), 
     plot = p, 
     device = cairo_pdf,
     width = 8, height = 20, units = "cm", dpi=300)
